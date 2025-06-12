29bcbdbc2370b88c718d0ed344f3dc3f7751cb3e
Kamran <Yousef.shaikhan@gmail.com>
2025-06-12 02:11:51 +0100

    created-mini-project-with-arrays-method
    I made the manipulating-arrays project. and used the shift, unshift, push and pop method

---
+++ 6.1-Manipulating-Arrays/scripts.js
@@ -0,0 +1,30 @@
+let array = [];
+
+function addElement() {
+    const inputValue = document.getElementById("inputField").value;
+    array.push(inputValue);
+    console.log(array);
+}
+
+function removeLastElement() {
+    array.pop();
+    console.log(array);
+}
+
+function addFirst(){
+    const inputElement = document.getElementById("inputField").value;// we can use the same input field for adding first element
+    // or we can create a new input field for adding first element, it's up to you this is a style choice - not a rule optioanl
+    // In a real application, you might want to use a different input field for adding the first element,
+    // but for simplicity, we will use the same input field
+     
+    //Each function has its own local scope.
+    //Variable names inside functions are independent — they don't have to match.
+    //You can name your variables whatever makes sense to you or makes the code clearer.
+    array.unshift(inputElement);
+    console.log(array);
+}
+
+function removeFirst() {
+    array.shift();
+    console.log(array); 
+}
